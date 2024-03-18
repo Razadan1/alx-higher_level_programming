c54db325212394d63834ca590b92fcea54230a9c
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-18 17:54:29 +0100

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return None
+        f_letter = None
     else:
-        return (len(sentence), sentence[0])
+        f_letter = sentence[0]
+    return (len(sentence), sentence[0])
