6f572f48f9440984c620e67eaf72a727850d717b
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-18 17:29:09 +0100

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for item in sentence:
-        if sentence == '':
-            return None
-        else:
-            return (len(sentence), item[0])
+    if sentence == '':
+        return None
+    else:
+        return (len(sentence), sentence[0])
