443fc5a3c059ebeec05b078b4320fdc8a68d509e
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-21 14:16:15 +0100

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-Args = len(sys.argv) - 1
-if Args == 1:
-    print("{} argument:".format(Args))
-    print("{}: {}".format(Args, sys.argv[1]))
-elif Args > 1:
-    print("{} arguments:".format(Args))
-    for i in range(1, Args + 1):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("{} arguments.".format(Args))
+if __name__ = "__main__":
+    import sys
+    Args = len(sys.argv) - 1
+    if Args == 1:
+        print("{} argument:".format(Args))
+        print("{}: {}".format(Args, sys.argv[1]))
+    elif Args > 1:
+        print("{} arguments:".format(Args))
+        for i in range(1, Args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments.".format(Args))
