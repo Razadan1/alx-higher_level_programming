b3c76130d6895a6ae6189e30d8d9a88ae2cb12f6
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-21 14:11:19 +0100

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
-Args = len(sys.argv) -1
-if  Args == 1:
+Args = len(sys.argv) - 1
+if Args == 1:
     print("{} arguement:".format(Args))
     print("{}: {}".format(Args, sys.argv[1]))
 elif Args > 1:
     print("{} arguements:".format(Args))
-    for i in range (1, Args + 1):
+    for i in range(1, Args + 1):
         print("{}: {}".format(i, sys.argv[i]))
 else:
     print("{} arguement.".format(Args))
