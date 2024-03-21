f571a9cad8eb027e40b9baa5a5fd0d9369fa7a3d
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-21 15:54:35 +0100

    Update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in sorted(dir(hidden_4)):
-        if i.startswith("__"):
-            continue
-        print(i)
+    for i in dir(hidden_4):
+        if not i[:2] == ("__"):
+            print(i)
