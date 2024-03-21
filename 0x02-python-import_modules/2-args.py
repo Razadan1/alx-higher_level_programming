b2835e67ead860b1d4738179319ada9c4ebb6e51
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-21 13:03:28 +0100

    Task 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python
 import sys
-
-# Get the number of arguments (excluding the script name)
-num_arguments = len(sys.argv) - 1
-
-# Print the number of arguments
-print("Number of argument(s):", num_arguments)
-
-# Check if arguments were provided
-if num_arguments > 0:
-    print("Arguments:")
-    
-    # Iterate over each argument and print its position and value
-    for i in range(1, num_arguments + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+Args = len(sys.argv) -1
+if  Args >= 1:
+    print("{} arguement:".format(Args))
+    print("{}: {}".format(sys.argv))
 else:
-    print("No arguments were passed.")
+    print("{} arguement.".format(Args))
