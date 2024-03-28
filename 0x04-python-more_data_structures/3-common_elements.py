bd651217e4c0f5f701f33e51d7f9b598654c490c
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-28 14:52:09 +0100

    Update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for item in set_1:
-        for item_2 in set_2:
-            if item == item_2:
-                common.append(item)
-    return common
+   return list(set_1 & set_2)
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+def update_dictionary(a_dictionary, key, value):
+    for item in a_dictionary:
+        if  item == key:
+            
