94617f29c04ae6e90eeaccbb7570711ac18f68fd
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-28 12:56:14 +0100

    Task 3 updated

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    common = []
     for item in set_1:
         for item_2 in set_2:
             if item == item_2:
-                return [item]
-            else:
-                None
+                common.append(item)
+    return common
