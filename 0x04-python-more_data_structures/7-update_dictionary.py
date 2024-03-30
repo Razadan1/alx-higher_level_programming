35b8d325cb38a2508d76d39dd34fae0c3e6f7151
Razadan1 <danmoleomomayowa@gmail.com>
2024-03-30 02:30:36 +0100

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
+#!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for item in a_dictionary:
-        if  item == key:
-            
+    a_dictionary[key] = value
+    return a_dictionary
