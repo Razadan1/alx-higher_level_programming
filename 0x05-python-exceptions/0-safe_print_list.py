8785960e35dd97e984892063f757d7c78a8b114b
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-05 15:32:54 +0100

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list[:x]:
+            count += 1
+            print(item,end='')
+        print()
+        return count
+    except TypeError:
+        print("Invalid Format")
+        pass
