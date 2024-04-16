20ac6a1b88d058beca4217d6996c469904c9a098
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-16 17:08:34 +0100

    Task 0 examined

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" A function that reads files """
+
+
 def read_file(filename=""):
+    """ This function reads anyfile """
     with open(filename, encoding="utf-8") as readfile:
-        print(readfile.read())
+        print(readfile.read(), end="")
