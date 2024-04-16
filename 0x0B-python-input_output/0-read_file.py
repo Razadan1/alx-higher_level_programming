047bc060a43245c7319ba8e39ced13b29fc307b3
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-16 17:01:47 +0100

    Task 0 update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(, encoding="utf-8") as readfile:
-        return (readfile.read())
+    with open(filename, encoding="utf-8") as readfile:
+        print(readfile.read())
