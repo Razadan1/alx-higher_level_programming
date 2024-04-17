ea141540dd479e5dba3e9c33e4fa8c203b09b3b6
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-17 17:14:54 +0100

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A function that returns a list of integers in pascal triangle"""
+
+def pascal_triangle(n):
+    """print pascal"""
+    p_triangle = [[0]*i for i in range(1, n+1)]
+    for i in range(n):
+        p_triangle[i][0] = 1
+        p_triangle[i][-1] = 1
+        for j in range(0, i//2):
+            p_triangle[i][j+1] = p_triangle[i-1][j] + p_triangle[i-1][j+1]
+            p_triangle[i][i-j-1] = p_triangle[i-1][j] + p_triangle[i-1][j+1]
+
+    return p_triangle
