7813a2fc576e17de2f7a9398634bac74fa047918
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-17 15:31:34 +0100

    Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+""" This function defines a student bypublic attributes"""
+
+
+
 class Student:
     def __init__(self, first_name, last_name, age):
+        """ The main function for instanciating the attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ This returns the dict format  of the object"""
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
