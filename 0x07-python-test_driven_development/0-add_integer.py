7a183f2d30586c02a7c7e3461b23f8d2058c5d99
Razadan1 <danmoleomomayowa@gmail.com>
2024-04-25 14:50:20 +0100

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ A function that adds two integers"""
 
+
 def add_integer(a, b=98):
     """ Adds two integers"""
     if type(a) != float and type(a) != int:
-         raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
     return int(a + b)
