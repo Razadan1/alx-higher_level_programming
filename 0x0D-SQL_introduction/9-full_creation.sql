cbd4afc723b030f9061ab11afed3e45fa0e15823
Razadan1 <danmoleomomayowa@gmail.com>
2024-05-21 17:46:02 -0700

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- creates a table second_table
+CREATE TABLE IF EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name) 
+VALUES(1, "John", 10);
+VALUES(2, "Alex", 3);
+VALUES(3, "Bob", 14);
+VALUES(4, "George", 8);
+
