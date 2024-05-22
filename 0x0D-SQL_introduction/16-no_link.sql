1eeabf4f4952af82c959800cc7a507a98cc0f5de
Razadan1 <danmoleomomayowa@gmail.com>
2024-05-21 18:44:07 -0700

    Task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
 -- lists all records of the table
-INSERT INTO second_table(name, score) VALUES("Aria", 18),
-("Aria", 12);
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC;
