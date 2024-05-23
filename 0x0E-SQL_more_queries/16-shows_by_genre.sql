625b07cc950a2dc1c8713fc78ab449f28834aaa1
Razadan1 <danmoleomomayowa@gmail.com>
2024-05-23 10:20:14 -0700

    Task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Lists all shows and all genres linkedd to that show from the database
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_
+show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC, t
+v_genres.name;
