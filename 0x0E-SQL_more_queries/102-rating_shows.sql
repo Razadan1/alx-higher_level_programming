6d533a02a02ef60cbabd91cbd2154c9f5b230219
Razadan1 <danmoleomomayowa@gmail.com>
2024-05-23 21:45:37 -0700

    Advanced Task 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,2 @@
+-- Lists all shows from the database by their ratings
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id GROUP BY title ORDER BY rating DESC;
