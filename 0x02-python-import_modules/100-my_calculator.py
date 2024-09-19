da78c5d0e5d5cde64431ba8d461471dcb9513fd6
Razadan1 <danmoleomomayowa@gmail.com>
2024-09-19 18:36:39 +0100

    Advanced 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,25 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if (len(sys.argv) - 1) != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     ops = {'+': add, '-': sub, '*': mul, '/': div}
 
-    if sys.argv[2] not in list(ops.keys()):
+    operator = sys.argv[2]
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, ops[sys.argv[2]](a,
-          b)))
+    # Perform the operation and store the result
+    result = ops[operator](a, b)
+
+    # Format the result to display as an integer if possible, else float
+    if operator == '/' and b != 0:
+        print(f"{a} {operator} {b} = {result:.1f}")  # Use float format for division
+    else:
+        print(f"{a} {operator} {b} = {int(result)}")  # Use integer format for other operations
